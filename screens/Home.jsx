d62b7111b11a168d20f144b8af749ce89428626d
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-08-03 21:57:08 +0300

    Changes to be committed: 	modified:   screens/Home.jsx

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context'
+import {SafeAreaView} from 'react-native-safe-area-context';
+import styles from './home.style';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 const Home = () => {
   return (
     <SafeAreaView>
-      <Text>Home page</Text>
+      <View style={styles.appBarWrapper}>
+        <View style={styles.appBar}>
+          <Ionicons name="location-outline" size={24} />
+          <Text style={styles.location}>Cairo - Egypt</Text>
+          <View style={{alignItems: 'flex-end'}}>
+            <View style={styles.cutCount}>
+              <Text style={styles.cutNumber}> 8 </Text>
+            </View>
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
