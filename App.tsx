79070a9437218fe2444de97a603d3c3dfd961ec7
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-08-03 12:30:30 +0300

    Changes to be committed: 	modified:   App.tsx 	modified:   react-native.config.js

--- App.tsx
+++ App.tsx
@@ -4,11 +4,15 @@ import {StyleSheet, Text, View} from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi there</Text>
+      <Text style={styles.Text}>Hi there</Text>
+      <Text style={styles.regText}>Hi there</Text>
+
       <StatusBar barStyle="dark-content" />
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  Text: {fontSize: 24, fontFamily: 'Poppins Bold'},
+  regText: {fontSize: 24},
 });
