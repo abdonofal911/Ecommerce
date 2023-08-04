96c3dae12666a47d3354c27321f9c0065b12102e
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-08-04 21:10:20 +0300

    Changes to be committed: 	modified:   screens/Search.jsx 	new file:   screens/search.style.js

--- screens/Search.jsx
+++ screens/Search.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import {Text, View} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context'
-
-
+import {Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import styles from './search.style';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {useNavigation} from '@react-navigation/native';
+import {COLORS, SIZES} from '../assets/constants';
 const Search = () => {
+  const navigation = useNavigation();
   return (
     <SafeAreaView>
-      <Text>Search page</Text>
+      <View style={styles.searchContainer}>
+        <TouchableOpacity>
+          <Ionicons
+            name="camera-outline"
+            size={SIZES.xLarge}
+            style={styles.searcIcon}
+          />
+        </TouchableOpacity>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            placeholder="What are you looking for ?"
+            style={styles.searchInput}
+            value=""
+            onPressIn={() => {
+              navigation.navigate('Search');
+            }}
+          />
+        </View>
+        <View>
+          <TouchableOpacity style={styles.searchBtn}>
+            <Feather name="search" color={COLORS.offwhite} size={24} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
