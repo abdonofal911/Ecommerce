7912ca86a6217309704472546d03d8485c0ff2c0
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-08-04 20:06:22 +0300

    Changes to be committed: 	modified:   components/home/welcome.jsx 	modified:   components/home/welcome.style.js

--- components/home/welcome.jsx
+++ components/home/welcome.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, TextInput, Touchable, TouchableOpacity, View} from 'react-native';
 import styles from './welcome.style';
 import {COLORS, SIZES} from '../../assets/constants';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {useNavigation} from '@react-navigation/native';
 
 const Welcome = () => {
+  const navigation = useNavigation();
   return (
     <View>
       <View style={styles.container}>
@@ -13,6 +17,31 @@ const Welcome = () => {
         <Text style={styles.welcometxt(COLORS.primary, 0)}>
           Luxurious Furniture
         </Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <TouchableOpacity>
+          <Feather name="search" size={24} style={styles.searcIcon} />
+        </TouchableOpacity>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            placeholder="What are you looking for ?"
+            style={styles.searchInput}
+            value=""
+            onPressIn={() => {
+              navigation.navigate('Search');
+            }}
+          />
+        </View>
+        <View>
+          <TouchableOpacity style={styles.searchBtn}>
+            <Ionicons
+              name="camera-outline"
+              size={SIZES.xLarge}
+              color={COLORS.offwhite}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
--- components/home/welcome.style.js
+++ components/home/welcome.style.js
@@ -11,6 +11,40 @@ const styles = StyleSheet.create({
     color: color,
     marginHorizontal: 12,
   }),
+  searchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.secondary,
+    borderRadius: SIZES.medium,
+    marginVertical: SIZES.medium,
+    height: 50,
+    marginHorizontal : SIZES.small
+  },
+  searcIcon: {
+    marginHorizontal: 10,
+    color: COLORS.gray,
+  },
+  searchWrapper: {
+    flex: 1,
+    backgroundColor: COLORS.secondary,
+    marginRight: 10,
+    borderRadius: SIZES.small,
+  },
+  searchInput: {
+    fontFamily: 'Poppins-Regular',
+    width: '100%',
+    height: '100%',
+    paddingHorizontal: SIZES.small,
+  },
+  searchBtn: {
+    width: 50,
+    backgroundColor: COLORS.primary,
+    height: '100%',
+    borderRadius: SIZES.medium,
+    alignItems: 'center',
+    justifyContent : 'center'
+  },
 });
 
 export default styles;
