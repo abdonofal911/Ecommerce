fcf35ea4430840374d1bf6d9f6fc4e1fac7e6221
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-08-06 15:13:28 +0300

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   screens/ProductDetails.jsx 	modified:   screens/ProductDetails.style.js 	modified:   yarn.lock

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {COLORS, SIZES} from '../assets/constants';
-
-import {View, TouchableOpacity, Image} from 'react-native';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import {View, TouchableOpacity, Image, Text} from 'react-native';
 import styles from './ProductDetails.style';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 const ProductDetails = ({navigation}) => {
+  const [count, setCount] = useState(1);
+
   return (
     <View style={styles.container}>
       <View style={styles.upperRow}>
@@ -21,6 +23,53 @@ const ProductDetails = ({navigation}) => {
         }}
         style={styles.image}
       />
+      <View style={styles.details}>
+        <View style={styles.titleRow}>
+          <Text style={styles.title}>Product Name</Text>
+          <View style={styles.priceWrapper}>
+            <Text style={styles.price}>{99.99}</Text>
+          </View>
+        </View>
+        <View style={styles.ratingRow}>
+          <View style={styles.rating}>
+            {[1, 2, 3, 4, 4].map(index => (
+              <Ionicons
+                key={Math.random() * 100}
+                name="star"
+                size={24}
+                color="gold"
+              />
+            ))}
+            <Text style={styles.ratingText}>(4.9)</Text>
+          </View>
+          <View style={styles.rating}>
+            <TouchableOpacity
+              onPress={() => {
+                if (count < 5) {
+                  setCount(count + 1);
+                }
+              }}>
+              <SimpleLineIcons name="plus" size={20} />
+            </TouchableOpacity>
+            <Text style={styles.ratingText}>({count})</Text>
+            <TouchableOpacity
+              onPress={() => {
+                if (count > 1) {
+                  setCount(count - 1);
+                }
+              }}>
+              <SimpleLineIcons name="minus" size={20} />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.descriptionwrapper}>
+          <Text style={styles.description}>description</Text>
+          <Text style={styles.descText}>
+            Lorem ipsum is placeholder text commonly used in the graphic, print,
+            and publishing industries for previewing layouts and visual mockups.{' '}
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
--- screens/ProductDetails.style.js
+++ screens/ProductDetails.style.js
@@ -3,6 +3,7 @@ import {COLORS, SIZES} from '../assets/constants';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: COLORS.lightWhite,
   },
   upperRow: {
     marginHorizontal: 20,
@@ -17,6 +18,69 @@ const styles = StyleSheet.create({
   image: {
     aspectRatio: 1,
     resizeMode: 'cover',
+  },
+  details: {
+    marginTop: -SIZES.large,
+    backgroundColor: COLORS.lightWhite,
+    width: SIZES.width,
+    borderTopLeftRadius: SIZES.medium,
+    borderTopRightRadius: SIZES.medium,
+  },
+  titleRow: {
+    marginHorizontal: 20,
+    paddingBottom: SIZES.small,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: SIZES.width - 44,
+    top: 20,
+  },
+  title: {
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: SIZES.large,
+  },
+  priceWrapper: {
+    backgroundColor: COLORS.secondary,
+    borderRadius: SIZES.large,
+  },
+  price: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: SIZES.large,
+    paddingHorizontal: 10,
+  },
+  ratingRow: {
+    paddingBottom: SIZES.small,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: SIZES.width - 10,
+    top: 5,
+    right: 11,
+  },
+  rating: {
+    top: SIZES.large,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginHorizontal: SIZES.large,
+  },
+  ratingText: {
+    color: COLORS.gray,
+    fontFamily: 'Poppins-Medium',
+  },
+  descriptionwrapper: {
+    marginTop: SIZES.large * 2,
+    marginHorizontal: SIZES.large - 2,
+  },
+  description: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: SIZES.large,
+  },
+  descText: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: SIZES.small,
+    textAlign: 'justify',
+    marginBottom: SIZES.small,
   },
 });
 
